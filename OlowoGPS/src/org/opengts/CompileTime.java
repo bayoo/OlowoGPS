b85376eb5a4bb8ea1e69255d4807cf26c2c9228c
Adebayo Olowoyeye <bayoolowoyeye@gmail.com>
2012-03-03 23:03:54 -0500

    Opened the Driver Admin Feature - Added Menu images for driver admin

--- OlowoGPS/src/org/opengts/CompileTime.java
+++ OlowoGPS/src/org/opengts/CompileTime.java
@@ -1,6 +1,6 @@
 package org.opengts;
 public class CompileTime
 {
-    // 2012/03/01 01:02:54 EST
-    public static final long COMPILE_TIMESTAMP = 1330581774L;
+    // 2012/03/03 23:00:55 EST
+    public static final long COMPILE_TIMESTAMP = 1330833655L;
 }
